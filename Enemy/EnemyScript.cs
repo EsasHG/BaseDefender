68ed2120841b7a87aaaca7a6a97c7c3dc328280d
Ludvig Risan <ludvig.risan@gmail.com>
2024-09-14 16:01:43 +0200

    Hit base
    enemies can now hit the base

--- Enemy/EnemyScript.cs
+++ Enemy/EnemyScript.cs
@@ -12,6 +12,20 @@ public partial class EnemyScript : CharacterBody2D
 	public override void _PhysicsProcess(double delta){
 		Velocity = -Position.Normalized() * Speed;
 		MoveAndSlide();
+
+		int collisionCount = GetSlideCollisionCount();
+
+		for (int i = 0; i < collisionCount; i++){
+			KinematicCollision2D collision = GetSlideCollision(i);
+			if(((Node2D)collision.GetCollider()) is Base target)
+			{
+				target.damage();
+				QueueFree();
+				break;
+			}
+			
+		}
+		
 	}
 
 	public void hit(int amount = 1){
