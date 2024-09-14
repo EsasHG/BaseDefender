726915a8efaa4cd9b717a97a639f6851c59eb855
Espen <slitwn@outlook.com>
2024-09-14 14:09:32 +0200

    Changed scene and started on attacking

--- Player/PlayerMovement.gd
+++ Player/PlayerMovement.gd
@@ -1,17 +1,19 @@
 extends CharacterBody2D
 
 
+
+const ATTACK_DISTANCE = 20.0
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta):
-	# Add the gravity.
+	# Add the g$"."ravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 	#	velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -29,5 +31,19 @@ func _physics_process(delta):
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	velocity = velocity.normalized()  * SPEED
-
+	
+	if Input.is_action_pressed("Attack"):
+		_attack()
+	if Input.is_action_just_released("Attack"):
+		$DamageSprite.visible = false
+	
 	move_and_slide()
+
+func _attack():
+	$DamageSprite.visible = true
+	var dmgVec = get_local_mouse_position().normalized()*ATTACK_DISTANCE
+	
+	$DamageSprite.position = dmgVec
+	$DamageSprite.rotation = dmgVec.angle()
+	#$DamageSprite.rotate(dmgVec.))
+	print_debug( get_local_mouse_position().normalized())
