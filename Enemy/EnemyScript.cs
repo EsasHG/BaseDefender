3edb8ab3fb66e2f877e52b3fde3d3bb242588c12
Ludvig Risan <ludvig.risan@gmail.com>
2024-09-14 14:19:16 +0200

    enemy health

--- Enemy/EnemyScript.cs
+++ Enemy/EnemyScript.cs
@@ -5,9 +5,22 @@ public partial class EnemyScript : CharacterBody2D
 {
 	[Export]
 	float Speed = 300.0f;
-
+	
+	[Export]
+	float health = 3;
+	
 	public override void _PhysicsProcess(double delta){
 		Velocity = -Position.Normalized() * Speed;
 		MoveAndSlide();
 	}
+
+	public void hit(int amount = 1){
+		health -= amount;
+
+		if (health <= 0){
+			QueueFree();
+		}
+	}
+	
+	
 }
