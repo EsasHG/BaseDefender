1b191a6b87b3bfc19450648c8123b880861cd3da
Ludvig Risan <ludvig.risan@gmail.com>
2024-09-15 11:47:01 +0200

    Director script

---
+++ Director/Director.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class Director : Node2D
+{
+	public static Director director;
+	
+	[Export]
+	public Spawner spawner;
+	
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready(){
+		director = this;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta){
+		
+	}
+}
